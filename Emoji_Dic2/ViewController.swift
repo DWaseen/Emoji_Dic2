a65e6541271d8767501bab10b67bedb8718972b9
Dan Waseen <dan.waseen1@gmail.com>
2016-11-19 14:01:51 -0600

    Initial Commit

---
+++ Emoji_Dic2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji_Dic2
+//
+//  Created by Dan Waseen on 11/19/16.
+//  Copyright © 2016 Dan Waseen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
